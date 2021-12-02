d01eb964a6ca9bbbe84e0c01e83a7aad6b40cf9a
Ozzie Neher <oz.neher@gmail.com>
2021-12-02 12:09:34 -0500

    cleanup day 01

--- lib/advent_of_code2021/day01.ex
+++ lib/advent_of_code2021/day01.ex
@@ -1,69 +1,35 @@
 defmodule AdventOfCode2021.Day01 do
-  @type input :: [integer]
-  @type three_measurement_window :: [integer]
-  @type offset :: integer
-  @type total :: integer
-  @type index :: integer
-
   alias AdventOfCode2021.InputReader
 
-  def handle_pt1 do
-    read_input()
-    |> parse_pt1()
+  defmodule Part01 do
+    def handle do
+      InputReader.read("day01_input.txt")
+      |> String.split("\n")
+      |> Enum.map(&String.to_integer/1)
+      |> parse()
+    end
+
+    def parse([head | tail]), do: parse(tail, head, 0)
+
+    def parse([head | tail], prev, total) when head > prev,
+      do: parse(tail, head, total + 1)
+
+    def parse([head | tail], _prev, total), do: parse(tail, head, total)
+    def parse([], _prev, total), do: total
   end
 
-  # Recursively loops through a list, returning how many
-  # elements are greater than the one previous to it
+  defmodule Part02 do
+    def handle do
+      InputReader.read("day01_input.txt")
+      |> String.split("\n")
+      |> Enum.map(&String.to_integer/1)
+      |> parse([])
+      |> Part01.parse()
+    end
 
-  @spec parse_pt1(input) :: total
-  defp parse_pt1(list), do: parse_pt1(list, 0, 0)
+    defp parse([one, two, three | tail], carry),
+      do: parse([two, three | tail], carry ++ [one + two + three])
 
-  @spec parse_pt1(input, total, index) :: total
-  defp parse_pt1(list, total, 0), do: parse_pt1(list, total, 1)
-
-  defp parse_pt1(list, total, index) when length(list) == index do
-    total
-  end
-
-  defp parse_pt1(list, total, index) do
-    prev = Enum.at(list, index - 1)
-    current = Enum.at(list, index)
-
-    new_total = if current > prev, do: total + 1, else: total
-
-    parse_pt1(list, new_total, index + 1)
-  end
-
-  ### PT 2
-
-  def handle_pt2 do
-    read_input()
-    |> parse_pt2()
-    |> parse_pt1()
-  end
-
-  # Recursively loops through a list, returning a new list where
-  # each item is the sum of the current offset + the next two items
-  # in the original list, stopping when their are no longer 3 items ahead
-  # of the offset
-
-  @spec parse_pt2(input) :: three_measurement_window
-  defp parse_pt2(list), do: parse_pt2(list, [], 0)
-
-  @spec parse_pt2(input, three_measurement_window, offset) :: three_measurement_window
-  defp parse_pt2(list, carry, offset) when length(list) - offset < 3, do: carry
-
-  defp parse_pt2(list, carry, offset) do
-    one = Enum.at(list, offset)
-    two = Enum.at(list, offset + 1)
-    three = Enum.at(list, offset + 2)
-
-    parse_pt2(list, carry ++ [one + two + three], offset + 1)
-  end
-
-  defp read_input do
-    InputReader.read("day01_input.txt")
-    |> String.split("\n")
-    |> Enum.map(&String.to_integer/1)
+    defp parse([_, _], carry), do: carry
   end
 end
