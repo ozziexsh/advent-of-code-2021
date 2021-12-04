d8c41bffe51e3c91448d047593ec041ba7088e4c
Ozzie Neher <oz.neher@gmail.com>
2021-12-04 15:01:40 -0500

    Day 03 part 02, needs refactoring

--- lib/advent_of_code2021/day03.ex
+++ lib/advent_of_code2021/day03.ex
@@ -40,4 +40,55 @@ defmodule AdventOfCode2021.Day03 do
     def invert([1 | tail], carry), do: invert(tail, carry ++ [0])
     def invert([], carry), do: carry
   end
+
+  defmodule Part02 do
+    def handle do
+      input =
+        InputReader.read("day03_input.txt")
+        |> String.split("\n")
+
+      oxygen_generator_rating = get_most_frequent(input, 0)
+      co2_scrubber_rating = get_least_frequent(input, 0)
+
+      oxygen_generator_rating * co2_scrubber_rating
+    end
+
+    def get_most_frequent([final], _index) do
+      {num, _} = Integer.parse(final, 2)
+      num
+    end
+
+    def get_most_frequent(list, index) do
+      most_frequent = count_frequency(list, index, {0, 0})
+
+      remaining = Enum.filter(list, fn str -> String.at(str, index) == most_frequent end)
+
+      get_most_frequent(remaining, index + 1)
+    end
+
+    def get_least_frequent([final], _index) do
+      {num, _} = Integer.parse(final, 2)
+      num
+    end
+
+    def get_least_frequent(list, index) do
+      most_frequent = count_frequency(list, index, {0, 0})
+
+      remaining = Enum.filter(list, fn str -> String.at(str, index) != most_frequent end)
+
+      get_least_frequent(remaining, index + 1)
+    end
+
+    def count_frequency([head | tail], index, {zeros, ones}) do
+      char = String.at(head, index)
+      count_frequency(tail, index, count_char(char, {zeros, ones}))
+    end
+
+    def count_frequency([], _index, {zeros, ones}) when zeros > ones, do: "0"
+    def count_frequency([], _index, {zeros, ones}) when ones > zeros, do: "1"
+    def count_frequency([], _index, {zeros, ones}) when zeros == ones, do: "1"
+
+    def count_char("0", {zeros, ones}), do: {zeros + 1, ones}
+    def count_char("1", {zeros, ones}), do: {zeros, ones + 1}
+  end
 end
